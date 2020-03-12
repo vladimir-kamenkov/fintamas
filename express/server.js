3cda25dd2fdca7fd0e075cf4424a70e1d8ad80c7
vladimir-kamenkov <vladimir.kamenkov@moneyman.ru>
2020-03-12 21:18:36 +0300

    add transactions

--- express/server.js
+++ express/server.js
@@ -1,9 +1,36 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TOKEN;
+
+// Created instance of TelegramBot
+const bot = new TelegramBot(token, {
+  polling: true
+});
+
+// Listener (handler) for showcasing different keyboard layout
+bot.onText(/\/transaction/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Select category of transaction', {
+    'reply_markup': {
+      'keyboard': [
+        ['apartment', 'applications'],
+        ['business', 'debts'],
+        ['entertainment', 'food'],
+        ['gifts', 'services'],
+        ['shopping', 'transport']
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+      force_reply: true,
+    }
+  });
+});
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -13,6 +40,7 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
